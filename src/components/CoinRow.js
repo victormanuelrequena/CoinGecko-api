36953951c8abb8a73d06cf2f214e8d21857e44c0
victor requena <requenadev1@gmail.com>
2021-08-15 13:57:10 -0400

    first commit

---
+++ src/components/CoinRow.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function CoinRow({ coin, index }) {
+	return (
+		<tr>
+			<td>{index}</td>
+			<td>
+			<img src={coin.image} alt={coin.name} style={{width: '3%'}} className="me-4 img-fluid" />
+			<span>
+			{coin.name}
+			<span className="ms-3 text-muted text-uppercase">{coin.symbol}</span>
+			</span>
+			</td>
+			<td>{coin.current_price}</td>
+			<td className={
+				coin.price_change_percentage_24h > 0
+				? 'text-success'
+				: 'text-danger'}
+				>
+				{coin.price_change_percentage_24h}
+			</td>
+			<td>{coin.total_volume}</td>
+		</tr>
+	)
+}
+
+export default CoinRow
